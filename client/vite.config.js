52b9f958a2324a57e87b8fd02ace75dc30a87e2f
priyam <kapadiyapriyam36@gmail.com>
2026-08-15 15:02:14 +0530

    generate icon

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'WorkLab',
         short_name: 'WorkLab',
